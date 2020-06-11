c245d441575f0d5bb8383e36169006de35cbff26
Micah <mcmanyika@gmail.com>
2020-06-11 10:52:36 -0500

    Debug false

--- chamisa/settings.py
+++ chamisa/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xf)kpm507f9-p82(a+anzbvagb89g(5q#r-7wyoo0a6bknab0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['167.99.12.126']
+DEBUG = False
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.99.12.126']
 
 
 # Application definition
