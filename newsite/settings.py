d2596c5f3d1238dcb1ba33f3c212a14f19bd5912
Micah <mcmanyika@gmail.com>
2020-09-17 15:46:20 -0500

    debug false

--- newsite/settings.py
+++ newsite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z5u_2#s7(hx_7zxv&wdgt*&htwpq63c!7os)1c61j=)x$u15(i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 if DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1', '165.22.17.129', 'www.nelsonchamisa.online',
                      'nelsonchamisa.online']
